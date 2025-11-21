0c3a9956a21d5b22c490afb9ddcd98498f016e12
sergiotech234 <sergio.herbla.4@educa.jcyl.es>
2025-11-21 20:28:20 +0100

    Update Divisor.java
    introduccion de datos

--- src/Divisor.java
+++ src/Divisor.java
@@ -1,4 +1,5 @@
 public class Divisor {
+    //creamos un metodo en el cual utilizamos una exception para que de un mensaje de error
     public double dividir(int a, int b) throws ArithmeticException {
         if (b == 0) {
             throw new ArithmeticException("Division by zero");
